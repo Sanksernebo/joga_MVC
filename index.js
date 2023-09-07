5646782875d98d6e065d79c17cbc2e4d1f4ff385
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-09-07 11:24:47 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname:'hbs',
     defaultLayout: 'main',
-    layoutDir:__dirname+'views/layouts/',
+    layoutDir:__dirname +'views/layouts/',
 }))
 // setup static public directory
 app.use(express.static('public'));
@@ -33,7 +33,23 @@ con.connect(function (err){
     console.log("connected to mydb");
 })
 
-//app stat point
-app.listen(3005, () => {
-    console.log('app is started at http://localhost:3005');
+
+
+// show all articles - index page
+app.get('/', (req,res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
+});
+
+
+//app start point
+app.listen(3000, () => {
+    console.log('app is started at http://localhost:3000');
 });
