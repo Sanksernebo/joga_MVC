f78bb914e0d971dd890835d5883601f1b0ba24b2
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-09-14 09:27:31 +0300

    display all articles using the article model

--- controllers/article.js
+++ controllers/article.js
@@ -1,33 +1,35 @@
 // import database connection
-const con = require('../utils/db');
+const Article = require('./models/article.model.js')
 
 // show all articles - index page
-const getAllArticles = ('/', (req,res) => {
-    let query = "SELECT * FROM article";
-    let articles = []
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        articles = result
-        res.render('index', {
-            articles: articles
-        })
-    })
-});
+const getAllArticles = (req,res) => {
+   Article.getAll((err,data) => {
+   if (err) {
+       res.status(500).send({
+message: err.message || 'Some error occurred retrieving articles data'
+       })
+   } else {
+       console.log(data)
+       res.render('index', {
+           articles:data
+       })
+   }
+   })
+};
 
 //show article by this slug
-const getArticleBySlug = ('/article/:slug',(req,res) => {
+const getArticleBySlug = (req,res) => {
     let query=`SELECT * FROM article 
     inner join author on slug="${req.params.slug}" AND article.author_id = author.id`
     let article
     con.query(query, (err, result) => {
         if (err) throw err;
         article = result
-        console.log(article)
         res.render('article', {
             article:article
         })
     })
-})
+};
 
 // export controller functions
 module.exports = {
